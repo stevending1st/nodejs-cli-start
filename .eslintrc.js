931c5c56b9e1d3138093df0f8879eb2317800f67
stevending1st <stevending1st@163.com>
2024-02-24 11:19:00 +0800

    feat: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "simple-import-sort"
+    ],
+    "rules": {
+        "simple-import-sort/exports": "error",
+        "simple-import-sort/imports": "error"
+    }
+}
